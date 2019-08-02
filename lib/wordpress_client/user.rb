5a6b556789fb0e9239420322ce79c43c3a8189ba
Jose Fernando Davila Orta <josefernandodavilaorta@MacBook-Pro-de-JoseFer.local>
2019-08-02 15:56:25 -0500

    Override comparison method for User class

--- lib/wordpress_client/user.rb
+++ lib/wordpress_client/user.rb
@@ -24,5 +24,34 @@ module WordpressClient
       @avatar_urls = avatar_urls
     end
 
+    # @api private
+    # Compares another instance. All attributes in this list must be equal for
+    # the instances to be equal:
+    #
+    # * +id+
+    # * +name+
+    # * +description+
+    # * +slug+
+    # * +avatar_urls+
+    #
+    # One must also not be a subclass of the other; they must be the exact same class.
+    def ==(other)
+      if other.is_a? User
+        other.class == self.class &&
+          other.id == id &&
+          other.name == name &&
+          other.description == description &&
+          other.slug == slug &&
+          other.avatar_urls == avatar_urls
+      else
+        super
+      end
+    end
+
+    # Shows a nice representation of the term type.
+    def inspect
+      "#<#{self.class} ##{id} #{name} (#{slug})>"
+    end
+
   end
 end
