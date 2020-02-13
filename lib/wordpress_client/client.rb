7f30fa67039679d25267e592ab1ce84701b79371
Fernando Davila <davila.jose23@gmail.com>
2020-02-13 12:03:25 -0600

    Change class name

--- lib/wordpress_client/client.rb
+++ lib/wordpress_client/client.rb
@@ -328,10 +328,10 @@ module WordpressClient
       "#<WordpressClient::Client #{connection.inspect}>"
     end
 
-    def related_posts(id, params)
+    def related_posts(id, params = {})
       params[:posts_per_page] ||=  10
       params[:fields] ||=  "ids"
-      connection.get(RelatedPost, "posts/#{id.to_i}", params, "related-posts-by-taxonomy/v1")
+      connection.get(Plugins::RelatedPostsByTaxonomy::RelatedPost, "posts/#{id.to_i}", params, "related-posts-by-taxonomy/v1")
     end
 
     private
